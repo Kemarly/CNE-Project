b89656e456de1f1d804cc624ca78cbca638b9047
KMarly D <44208106+Kemarly@users.noreply.github.com>
2024-01-29 20:53:36 -0500

    the tildas

--- CNE Project/CNE Project.cpp
+++ CNE Project/CNE Project.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <thread>
+#include <map>
+#ifndef CNE Project
+#define CNE Project
 #include <vector>
 #include <winsock2.h>
 #include <WS2tcpip.h>
@@ -8,17 +11,24 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS 
 using namespace std;
 
+map<string, SOCKET> activeClients;
+map<string, string> userCredentials;
+vector<string> publicMessages;
+
 int tcp_recv_whole(SOCKET s, char* buf, int len);
 int tcp_send_whole(SOCKET skSocket, const char* data, uint16_t length);
 void ServerCode(int port, int capacity, char commandChar);
-void ClientCode();
+void HandleCommand(const string& command, SOCKET clientSocket);
+void ProcessLogin(const string& username, const string& password, SOCKET clientSocket);
+void BroadcastMessage(const string& message, SOCKET senderSocket);
+void SendClientList(SOCKET clientSocket);
+void SavePublicMessage(const string& message);
 
 const int MAX_CLIENTS = 5;
 
 int tcp_recv_whole(SOCKET s, char* buf, int len)
 {
 	int total = 0;
-
 	do
 	{
 		int ret = recv(s, buf + total, len - total, 0);
@@ -28,7 +38,6 @@ int tcp_recv_whole(SOCKET s, char* buf, int len)
 			total += ret;
 
 	} while (total < len);
-
 	return total;
 }
 
@@ -36,7 +45,6 @@ int tcp_send_whole(SOCKET skSocket, const char* data, uint16_t length)
 {
 	int result;
 	int bytesSent = 0;
-
 	while (bytesSent < length)
 	{
 		result = send(skSocket, data + bytesSent, length - bytesSent, 0);
@@ -46,49 +54,9 @@ int tcp_send_whole(SOCKET skSocket, const char* data, uint16_t length)
 
 		bytesSent += result;
 	}
-
 	return bytesSent;
 }
 
-void main()
-{
-	//winsock
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0) { cout << "Winstock not initialize." << endl; return; }
-	int choice;
-	do
-	{
-		printf("Would you like to Create a Server or Client?\n");
-		printf("1> Server\n");
-		printf("2> Client\n");
-		cin >> choice;
-	} while (choice != 1 && choice != 2);
-
-	//Server
-	if (choice == 1)
-	{
-		int port, capacity;
-		char commandChar;
-		cout << "Port: ";
-		cin >> port;
-		cout << "Capacity: ";
-		cin >> capacity;
-		cout << "Command character: ";
-		cin >> commandChar;
-		ServerCode(port, capacity, commandChar);
-	}
-
-	//Client
-	else if (choice == 2)
-	{
-		ClientCode();
-	}
-
-	//shutdown winsock
-	WSACleanup();
-}
 void ServerCode(int port, int capacity, char commandChar)
 {
 	//socket
@@ -96,11 +64,6 @@ void ServerCode(int port, int capacity, char commandChar)
 	if (listening == INVALID_SOCKET) { cout << "Socket not created." << endl; return; }
 
 	//bind
-	/*sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(36000);
-	hint.sin_addr.S_un.S_addr = INADDR_ANY;
-	bind(listening, (sockaddr*)&hint, sizeof(hint));*/
 	sockaddr_in serverAddr;
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(port);
@@ -109,6 +72,7 @@ void ServerCode(int port, int capacity, char commandChar)
 	{
 		cout << "Bind failed." << endl;
 		closesocket(listening);
+		WSACleanup();
 		return;
 	}
 
@@ -151,6 +115,74 @@ void ServerCode(int port, int capacity, char commandChar)
 
 	//close sock
 	closesocket(clientSocket);
+}
+
+void HandleCommand(const string& command, SOCKET clientSocket)
+{
+	if (command.empty()) return;
+
+	char commandChar = command[0];
+	string args = command.substr(1);
+	switch (commandChar)
+	{
+	case '~':
+		if (args.find("register") == 0)
+		{
+			// ~register
+			size_t spacePos = args.find(' ');
+			if (spacePos != string::npos)
+			{
+				string username = args.substr(9, spacePos - 9);
+				string password = args.substr(spacePos + 1);
+			}
+		}
+		else if (args.find("login") == 0)
+		{
+			//~login
+			size_t spacePos = args.find(' ');
+			if (spacePos != string::npos)
+			{
+				string username = args.substr(6, spacePos - 6);
+				string password = args.substr(spacePos + 1);
+				void ProcessLogin(const string & username, const string & password, SOCKET clientSocket);
+			}
+		}
+		else if (args.find("send") == 0)
+		{
+			void SendClientList(SOCKET clientSocket);
+		}
+		else if (args.find("getlist") == 0)
+		{
+			// ~getlist .
+		}
+		else if (args.find("logout") == 0)
+		{
+			// ~logout
+		}
+		else
+		{
+			
+		}
+		break;
+	default:
+		BroadcastMessage(command, clientSocket); break;
+	}
+}
+
+void ProcessLogin(const string& username, const string& password, SOCKET clientSocket)
+{
+}
+
+void BroadcastMessage(const string& message, SOCKET senderSocket)
+{
+}
+
+void SendClientList(SOCKET clientSocket)
+{
+}
+
+void SavePublicMessage(const string& message)
+{
 }
 
 void ClientCode(void)
@@ -200,5 +232,45 @@ void ClientCode(void)
 
 	//close sock
 	closesocket(clientSocket);
-
 }
+int main()
+{
+	//winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0) { cout << "Winstock not initialize." << endl; return; }
+	int choice;
+	do
+	{
+		printf("Would you like to Create a Server or Client?\n");
+		printf("1> Server\n");
+		printf("2> Client\n");
+		cin >> choice;
+	} while (choice != 1 && choice != 2);
+
+	//Server
+	if (choice == 1)
+	{
+		int port, capacity;
+		char commandChar;
+		cout << "Port: ";
+		cin >> port;
+		cout << "Capacity: ";
+		cin >> capacity;
+		cout << "Command character: ";
+		cin >> commandChar;
+		ServerCode(port, capacity, commandChar);
+	}
+
+	//Client
+	else if (choice == 2)
+	{
+		ClientCode();
+	}
+
+	//shutdown winsock
+	WSACleanup();
+	return 0;
+}
+#endif CNE Project
